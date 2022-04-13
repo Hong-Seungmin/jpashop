6cb436325ccba157331373d5109e4688a95ee515
Seungmin <globalsh1@gmail.com>
2022-04-13 22:53:10 +0900

    회원 도메인 개발

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,59 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("Kim");
+        //when
+        Long memberId = memberService.join(member);
+
+        //then
+        em.flush();
+        Assertions.assertEquals(member,memberRepository.findOne(memberId));
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("Kim");
+
+        Member member2 = new Member();
+        member2.setName("Kim");
+
+        //when
+        memberService.join(member1);
+//        memberService.join(member2);
+
+        //then
+        Assertions.assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+//        Assert.fail("예외가 발생해야 한다.");
+    }
+}
